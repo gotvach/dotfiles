0bd2e35d766583468c4ecec42344098ecd6351ab
Grant Hammond <grant.hammond@digital.homeoffice.gov.uk>
2020-08-26 18:08:52 +0100

    emacs config updates for journaling and for v26

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,15 +8,25 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(auto-complete-mode t t)
+ '(auto-revert-verbose nil)
  '(column-number-mode t)
  '(compilation-message-face (quote default))
  '(custom-enabled-themes (quote (cyberpunk)))
  '(custom-safe-themes
    (quote
-    ("59e82a683db7129c0142b4b5a35dbbeaf8e01a4b81588f8c163bd255b76f4d21" "d1cc05d755d5a21a31bced25bed40f85d8677e69c73ca365628ce8024827c9e3" "d6922c974e8a78378eacb01414183ce32bc8dbf2de78aabcc6ad8172547cb074" "235dc2dd925f492667232ead701c450d5c6fce978d5676e54ef9ca6dd37f6ceb" "38e64ea9b3a5e512ae9547063ee491c20bd717fe59d9c12219a0b1050b439cdd" "e64111716b1c8c82638796667c2c03466fde37e69cada5f6b640c16f1f4e97df" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" default)))
+    ("b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" "2d835b43e2614762893dc40cbf220482d617d3d4e2c35f7100ca697f1a388a0e" "6bc387a588201caf31151205e4e468f382ecc0b888bac98b2b525006f7cb3307" "d1cc05d755d5a21a31bced25bed40f85d8677e69c73ca365628ce8024827c9e3" "d6922c974e8a78378eacb01414183ce32bc8dbf2de78aabcc6ad8172547cb074" "235dc2dd925f492667232ead701c450d5c6fce978d5676e54ef9ca6dd37f6ceb" "38e64ea9b3a5e512ae9547063ee491c20bd717fe59d9c12219a0b1050b439cdd" "e64111716b1c8c82638796667c2c03466fde37e69cada5f6b640c16f1f4e97df" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" default)))
+ '(desktop-save-mode t)
+ '(face-font-family-alternatives
+   (quote
+    (("Monospace" "courier" "fixed" "DejaVu Sans Mono")
+     ("courier" "CMU Typewriter Text" "fixed")
+     ("Sans Serif" "helv" "helvetica" "arial" "fixed")
+     ("helv" "helvetica" "arial" "fixed"))))
  '(fci-rule-color "#49483E")
  '(flymake-gui-warnings-enabled nil)
+ '(font-use-system-font nil)
+ '(global-auto-revert-mode t)
+ '(gnus-select-method (quote (nntp "news.gmane.org")))
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
@@ -36,11 +46,11 @@
  '(hl-line-mode t t)
  '(icon-mode nil)
  '(ido-mode (quote both) nil (ido))
- '(initial-buffer-choice "~/.emacs.d/init.el")
  '(linum-format " %3d ")
  '(linum-mode t t)
  '(magit-diff-use-overlays nil)
  '(menu-bar-mode nil)
+ '(minibuffer-message-timeout 5 t)
  '(ns-antialias-text nil)
  '(org-agenda-custom-commands
    (quote
@@ -52,21 +62,18 @@
      ("N" "NEXT in queue"
       ((todo "NEXT" nil))
       nil nil))))
+ '(org-agenda-files nil)
  '(org-confirm-babel-evaluate nil)
- '(package-archives
-   (quote
-    (("gnu" . "https://elpa.gnu.org/packages/")
-     ("melpa-stable" . "https://stable.melpa.org/packages/")
-     ("melpa" . "https://melpa.org/packages/"))))
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (smex smartparens s revive request multiple-cursors haskell-snippets flymake-puppet flycheck-package flycheck-haskell expand-region evil-surround dummyparens cyberpunk-theme company auto-complete)))
+    (org-journal-list org-journal auto-org-md flycheck-mmark flymd markdown-mode markdown-toc visual-fill-column rotate dante intero haskell-snippets evil magit magit-gitflow smex smartparens s request multiple-cursors flymake-puppet flycheck-package expand-region evil-surround dummyparens cyberpunk-theme company auto-complete)))
  '(puppet-lint-command
    "puppet-lint --no-autoloader_layout-check --with-context --log-format \"%{path}:%{line}: %{kind}: %{message} (%{check})\"")
+ '(revert-without-query (quote (".*")))
  '(server-mode t)
  '(show-paren-mode t)
- '(startup-screen-inhibit-startup-screen t t)
+ '(split-width-threshold 80)
  '(syslog-debug-face
    (quote
     ((t :background unspecified :foreground "#A1EFE4" :weight bold))))
@@ -82,6 +89,7 @@
  '(syslog-warn-face
    (quote
     ((t :background unspecified :foreground "#FD971F" :weight bold))))
+ '(toggle-scroll-bar t)
  '(tool-bar-mode nil)
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
@@ -105,7 +113,6 @@
      (340 . "#2790C3")
      (360 . "#66D9EF"))))
  '(vc-annotate-very-old-color nil)
- '(vc-follow-symlinks nil)
  '(weechat-color-list
    (quote
     (unspecified "#272822" "#49483E" "#A20C41" "#F92672" "#67930F" "#A6E22E" "#968B26" "#E6DB74" "#21889B" "#66D9EF" "#A41F99" "#FD5FF0" "#349B8D" "#A1EFE4" "#F8F8F2" "#F8F8F0"))))
@@ -114,18 +121,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#1d1f27" :foreground "#fff9ec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
 
 ;; Other packages
 (setq package-list
       '(
         auto-complete
+        company
+        cyberpunk-theme
         dummyparens
         evil
         expand-region
+        flycheck-package
+        flymake-puppet
         haskell-snippets
         multiple-cursors
-        revive
+        org-journal
         s
         smartparens
         smex
@@ -138,6 +149,9 @@
 ;;   (setq package-pinned-packages
 ;;         '((ghc . "melpa-stable"))))
 
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -149,7 +163,7 @@
 (require 'org-install)
 (org-babel-load-file
  (expand-file-name "settings.org"
-		   user-emacs-directory))
+                   user-emacs-directory))
 
 ;;;;;;;;;;;;;;;;;;;;;
 
@@ -158,6 +172,3 @@
 (global-set-key (kbd "M-x") 'smex)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-(require 'revive)
-(resume)
